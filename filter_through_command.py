7076ba30e1996b3f05da141f18df6ebe1fabbadd
Jeff Triplett <jeff.triplett@gmail.com>
2011-09-29 12:47:31 -0700

    The commit message was empty

--- filter_through_command.py
+++ filter_through_command.py
@@ -1,6 +1,7 @@
 # saved from: http://pastie.org/private/bclbdgxzbkb1gs2jfqzehg
 
-import sublime, sublimeplugin
+import sublime
+import sublimeplugin
 import subprocess
 
 class RunExternalCommand(sublimeplugin.TextCommand):
@@ -8,7 +9,7 @@ class RunExternalCommand(sublimeplugin.TextCommand):
     Runs an external command with the selected text,
     which will then be replaced by the command output.
     """
-    
+
     def run(self, view, args):
         if view.sel()[0].empty():
             # nothing selected: process the entire file
@@ -19,11 +20,11 @@ class RunExternalCommand(sublimeplugin.TextCommand):
 
         p = subprocess.Popen(
             args,
-            shell   = True,
-            bufsize = -1,
-            stdout  = subprocess.PIPE,
-            stderr  = subprocess.PIPE,
-            stdin   = subprocess.PIPE)
+            shell=True,
+            bufsize=-1,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE)
 
         output, error = p.communicate(view.substr(region).encode('utf-8'))
 
